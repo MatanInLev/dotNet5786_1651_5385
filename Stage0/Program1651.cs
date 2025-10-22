73c82a89009d11776979b01afa0c5dcce7ea6498
Matan Carmel <mcarmel@g.jct.ac.il>
2025-10-22 16:33:26 +0300

    stage0

---
+++ Stage0/Program1651.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome1651();
+        Welcome7440();
+        System.Console.ReadKey();
+    }
+
+    static partial void Welcome7440();
+    private static void Welcome1651()
+    {
+        System.Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        System.Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+}
