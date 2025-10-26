82d66d837e899f7cfec800b7871c4cbb8eed449f
Izaac23 <zibiaaron@gmail.com>
2025-10-26 17:40:17 +0200

    new change (Stage 0)

--- Stage0/Program1651.cs
+++ Stage0/Program1651.cs
@@ -1,17 +1,20 @@
-﻿partial class Program
+﻿namespace Stage0
 {
-    private static void Main(string[] args)
+        partial class Program
     {
-        Welcome1651();
-        Welcome5385();
-        System.Console.ReadKey();
-    }
+        private static void Main(string[] args)
+        {
+            Welcome1651();
+            Welcome5385();
+            System.Console.ReadKey();
+        }
 
-    static partial void Welcome5385();
-    private static void Welcome1651()
-    {
-        System.Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        System.Console.WriteLine("{0}, welcome to my first console application", name);
-    }
+        static partial void Welcome5385();
+        private static void Welcome1651()
+        {
+            System.Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            System.Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+}
 }
